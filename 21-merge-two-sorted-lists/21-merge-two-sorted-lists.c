f3da611f04021a17a71027aa148a76f844addfd3
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-07-14 20:17:23 +0530

    Time: 6 ms (52.39%), Space: 6.2 MB (26.10%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    struct ListNode reslist;
+    struct ListNode *res=&reslist;
+    if(list1==NULL)
+        return list2;
+    if(list2==NULL)
+        return list1;
+    while(list1!=NULL && list2!= NULL)
+    {
+        if(list1->val<=list2->val)
+        {
+            res->next=list1;
+            list1=list1->next;
+        }
+        else
+        {
+            res->next=list2;
+            list2=list2->next;
+        }
+        res=res->next;
+    }
+    if(list1==NULL && list2 != NULL){
+        res->next=list2;
+    }
+    else if(list2==NULL && list1 != NULL){
+        res->next=list1;
+    }
+    return reslist.next;
+}   
