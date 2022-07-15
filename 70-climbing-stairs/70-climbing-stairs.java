aa12b8924983fefe2aba76acad04a34d68823bfb
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-07-15 09:17:36 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (7.29%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,9 @@
+class Solution {
+    int dp[]=new int[46];
+    public int climbStairs(int n) {
+    if(dp[n]!=0) return dp[n];
+    if(n==1 || n==2) return n;
+    dp[n]=climbStairs(n-1)+climbStairs(n-2);
+    return dp[n];
+    }
+}
