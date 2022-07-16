d2aeb99b3a4c74ca305e526674a039dd584b5fc8
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-07-16 12:02:10 +0530

    Time: 86 ms (19.05%), Space: 14.4 MB (89.03%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if target in sum(matrix,[]):
+            return True
+        else:
+            return False     
+        
+        
