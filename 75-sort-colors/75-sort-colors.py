4e521068b98f83fc479996d0f82ab49327375c8f
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-07-18 12:15:00 +0530

    Time: 72 ms (10.37%), Space: 13.8 MB (63.60%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -2,7 +2,7 @@ class Solution:
     def sortColors(self, nums: List[int]) -> None:
         temp = 0
         for i in range(len(nums)):
-            for j in range(len(nums)- 1):
+            for j in range(len(nums)-i- 1):
                 if nums[j] >= nums[j+1]:
                     temp = nums[j]
                     nums[j] = nums[j+1]
