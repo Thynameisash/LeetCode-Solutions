c200a74da03886404895a220ffe844a8c26137f2
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-08-09 11:41:21 +0530

    Time: 90 ms (55.85%), Space: 13.9 MB (74.26%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -4,7 +4,7 @@ class Solution:
 #       [1,100,1,1,1,100,1,1,100,1]0
         cost.append(0)
         for i in range(len(cost)-3,-1,-1):
-            cost[i] = min(cost[i]+cost[i+1] ,cost[i]+cost[i+2])
+            cost[i] += min(cost[i+1] ,cost[i+2])
         
         return min(cost[0],cost[1])
         
