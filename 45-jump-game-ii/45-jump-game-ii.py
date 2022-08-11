4842bed3b057807383de5b231b452092f7799873
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-08-11 13:44:32 +0530

    Time: 170 ms (79.56%), Space: 15 MB (59.20%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,13 +1,19 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        curr=0
-        jumps=0
-        maxx=0
-        nlen=len(nums)-1
-        for i in range(nlen):
-            if i+nums[i]>=maxx:
-                maxx=i+nums[i]
-            if i==curr:
-                jumps+=1
-                curr=maxx
-        return jumps
+        # to keep the count of number of jumps
+        jump = 0
+        # to creat a window and initially window is 0
+        l = r = 0
+        # run the loop while r does not reach to the end of nums array
+        while r < len(nums) - 1:
+            # to search of the farther we can reach from the current window
+            farthest = 0
+            for i in range(l,r + 1):
+                farthest = max(farthest, i + nums[i])
+            # once the farthest is found change l to r + 1 and r to the farthest to create a new window
+            l = r + 1
+            r = farthest
+            # increment the count as we make 1 jump
+            jump += 1
+        # once loop is done return the count
+        return jump
