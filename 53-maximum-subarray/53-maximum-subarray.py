c4fbecf1cef51c614e4172fbc2df4792fe24ac4b
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-08-12 09:22:20 +0530

    Time: 1078 ms (55.60%), Space: 28.4 MB (16.08%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,9 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         currsum, overallsum = nums[0], nums[0]
+        
         if len(nums)==1: return nums[0]
+        
         for i in range (1,len(nums)):
             if currsum>=0: currsum+=nums[i]
             else: currsum=nums[i]
             if currsum>overallsum: overallsum=currsum
+        
         return overallsum
