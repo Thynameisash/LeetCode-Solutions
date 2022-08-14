1550fb92f1d09ec33b8e941a26212dd6dd1fe28d
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-08-14 21:38:56 +0530

    Time: 8 ms (8.09%), Space: 65.7 MB (8.45%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,25 +1,24 @@
-import java.util.Collections;
 class Solution {
     public int maxScoreSightseeingPair(int[] nums) {
-        int sum=0,diff=0;
         int [] ai=new int [nums.length];
         int [] aj=new int [nums.length];
         int n=nums.length-1;
         ai[0]=nums[0];
         aj[n]=nums[n]-n;
+        
         for(int i=1;i<n;i++){
             ai[i]=Math.max(ai[i-1],nums[i]+i);
         }
+        
         for(int j=n-1;j>0;j--){
             aj[j]=Math.max(aj[j+1],nums[j]-j);
         }
+        
         int max=0;
         for(int i=0;i<n;i++){
             max=Math.max(ai[i]+aj[i+1],max);
         }
         
-        System.out.print(Arrays.toString(ai));
-        System.out.print(Arrays.toString(aj));
         return max;
     }
 }
