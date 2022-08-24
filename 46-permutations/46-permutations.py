cb54f215e63cc570375060684384205160183b63
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-08-24 14:04:04 +0530

    Time: 70 ms (34.97%), Space: 14 MB (84.57%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        visited=[False]*len(nums)
+        print(visited)
+        def bt(res,curr,nums,visited):
+            if len(curr)==len(nums):
+                res.append(curr.copy())
+                return
+                
+            for i in range(len(nums)):
+                if visited[i]==True: continue
+                curr.append(nums[i])
+                visited[i]=True
+                bt(res,curr,nums,visited)
+                visited[i]=False
+                curr.pop()
+                    
+                    
+        bt(res,[],nums,visited)
+        return res
+        
