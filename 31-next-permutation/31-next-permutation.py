187a1e068ee8576e7928640ad69f0f710be57f28
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2022-08-26 21:32:36 +0530

    Time: 78 ms (25.96%), Space: 13.8 MB (74.85%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -2,21 +2,18 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         if len(nums)==1: return
         if len(set(nums)) ==1: return
+        
         flag =0
         for i in range(len(nums)-1,0,-1):
             if nums[i-1]<nums[i]:
                 numindex=i-1
                 flag=1
                 break
-            # elif nums[i-1]==nums[0]:
-            #     nums.sort()
-            #     return
-        print(flag)
+            
         if flag==0: 
             nums.sort()
             return
             
-        print(nums[numindex])
         mindif=inf
         for j in range(numindex,len(nums)):
             if nums[j]>nums[numindex]:
