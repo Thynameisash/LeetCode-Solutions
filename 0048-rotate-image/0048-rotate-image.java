245bdd3dd12291b6ddcf6f4ed0a152ce09707d51
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2023-11-25 13:00:17 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (56.21%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int s=0,e=n-1;
+        while(s < e){
+            int[] temp = matrix[s];
+            matrix[s] = matrix[e];
+            matrix[e] = temp;
+            s++; e--;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+    }
+}
