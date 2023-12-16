2ae9995660461b1f9499a058f0bf50e03a6e454b
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2023-12-16 13:45:56 +0530

    Time: 1 ms (98.94%), Space: 46.9 MB (37.05%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -1,49 +1,37 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
 public class Solution {
     public ListNode getIntersectionNode(ListNode l1, ListNode l2) {
-        ListNode d1=l1;
-        ListNode d2=l2;
-        int count1=0;
-        int count2=0;
-        ListNode len1=l1;
-        ListNode len2=l2;
-        while(len1!=null){
+        int count1 = 0, count2 = 0;
+        ListNode d1 = l1, d2 = l2;
+
+        // Count nodes in l1
+        while (l1 != null) {
             count1++;
-            len1=len1.next;
+            l1 = l1.next;
         }
-        while(len2!=null){
+        // Count nodes in l2
+        while (l2 != null) {
             count2++;
-            len2=len2.next;
+            l2 = l2.next;
         }
-        if(count1>count2){
-            int diff=count1-count2;
-            for(int i=0;i<diff;i++){
-                d1=d1.next;
-            }
-        }
-        if(count2>count1){
-            int diff=count2-count1;
-            for(int i=0;i<diff;i++){
-                d2=d2.next;
-            }
-        }
-        while(d1!=null && d2!=null){
-            if(d1==d2){
-                return d1;
+        // Reset l1 and l2 to the beginning
+        l1 = d1;
+        l2 = d2;
+        // Adjust the starting points based on the difference in counts
+        int diff = Math.abs(count1 - count2);
+        for (int i = 0; i < diff; i++) {
+            if (count1 > count2) {
+                l1 = l1.next;
             } else {
-                d1=d1.next;
-                d2=d2.next;
+                l2 = l2.next;
             }
+        }
+        // Find the intersection node
+        while (l1 != null && l2 != null) {
+            if (l1 == l2) {
+                return l1;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
         }
         return null;
     }
