5f8b09b7f31f8b53bfdf87efd35db045c81b763c
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-01-10 21:11:25 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (13.94%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    int dp[]=new int[46];
+    public int fib(int n) {
+        if(dp[n]!=0) return dp[n];
+        if(n==0 || n==1) return n;
+        
+        dp[n]=fib(n-1)+fib(n-2);
+        return dp[n];
+    }
+   
+}
