c506df2c3b62854d406fb1a78e7fb8609e47dc4f
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-01-12 21:16:07 +0530

    Time: 50 ms (5.96%), Space: 44.6 MB (5.81%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -7,18 +7,23 @@ class Solution {
         al.add('i');
         al.add('o');
         al.add('u');
+        al.add('A');
+        al.add('E');
+        al.add('I');
+        al.add('O');
+        al.add('U');
         
         int half = s.length()/2;
         char [] str = s.toCharArray();
         for(int i=0;i<half;i++){
-            if(al.contains(Character.toLowerCase(str[i]))){
+            if(al.contains(str[i])){
                 System.out.print(str[i]);
                 count1++;
             }
         }
        
        for(int i=half;i<s.length();i++){
-            if(al.contains(Character.toLowerCase(str[i]))){
+            if(al.contains(str[i])){
                 System.out.print(str[i]);
                 count2++;
             }
