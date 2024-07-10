3b265d07cdc3e773c78e70898982e216a272f2e1
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-07-10 19:25:12 +0530

    Time: 46 ms (77.86%), Space: 16.8 MB (36.19%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0 
+        
+        for i in logs:
+            if (i == "../") :
+                count-=1
+            elif (i == "./") :
+                continue
+            else:
+                count+=1
+            if count < 0 :
+                count=0
+                
+        return count
