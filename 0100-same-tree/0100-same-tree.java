ee9d0db0309285825f71dcc4f604af06001d5b8f
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-07-16 19:28:10 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (86.19%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,14 +13,22 @@
  *     }
  * }
  */
+
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-       if(p==null && q==null)
-           return true;
-        if (p==null || q==null)
+        return helper(p, q);
+    }
+    
+    boolean helper(TreeNode p, TreeNode q){
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
             return false;
-        if (p.val!=q.val)
+        }
+        if (p.val != q.val) {
             return false;
-        return (isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
+        }
+        return helper(p.left, q.left) && helper(p.right, q.right);
     }
 }
