250d27bd57a270891be5a2b293a0b243d86537c3
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-07-21 17:47:05 +0530

    Time: 2 ms (100.00%), Space: 41 MB (100.00%) - LeetHub

---
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minChanges(int n, int k) {    
+        
+        if ((n & k) != k){
+            return -1;
+        }
+        
+        String binaryN = Integer.toBinaryString(n);
+        String binaryK = Integer.toBinaryString(k);
+        
+        int maxLength = Math.max(binaryN.length(), binaryK.length());
+
+        while (binaryN.length() < maxLength) { 
+            binaryN = "0" + binaryN;
+        }
+
+        while (binaryK.length() < maxLength) {
+            binaryK = "0" + binaryK;
+        }
+        
+        char[] nArray = binaryN.toCharArray();
+        char[] kArray = binaryK.toCharArray();
+        
+        int count = 0;
+        
+        for (int i = 0; i < nArray.length; i++) {
+            if (nArray[i] != kArray[i] && nArray[i]=='1') {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
