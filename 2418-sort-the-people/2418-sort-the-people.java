d71642903db55e58d3942109fa6961dd00bec364
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-07-23 20:50:43 +0530

    Time: 7 ms (90.39%), Space: 45.7 MB (5.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,19 +1,25 @@
 class Solution {
 
     public String[] sortPeople(String[] names, int[] heights) {
-        int nop = names.length;
-        Map<Integer, String> hMap = new HashMap<>();
-        for (int i = 0; i < nop; i++) {
-            hMap.put(heights[i], names[i]);
+        int numberOfPeople = names.length;
+
+        // Create a map to store height-name pairs
+        Map<Integer, String> heightToNameMap = new HashMap<>();
+
+        // Populate the map with height as key and name as value
+        for (int i = 0; i < numberOfPeople; i++) {
+            heightToNameMap.put(heights[i], names[i]);
         }
 
         Arrays.sort(heights);
 
-        String[] sortedNames = new String[nop];
+        String[] sortedNames = new String[numberOfPeople];
 
         // Populate sortedNames array in descending order of height
-        for (int i = nop - 1; i >= 0; i--) {
-            sortedNames[nop - i - 1] = hMap.get(heights[i]);
+        for (int i = numberOfPeople - 1; i >= 0; i--) {
+            sortedNames[numberOfPeople - i - 1] = heightToNameMap.get(
+                heights[i]
+            );
         }
 
         return sortedNames;
