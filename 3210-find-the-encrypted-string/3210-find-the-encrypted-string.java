82b73fa1cdc39c6aa42177926c8170b08d252159
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-08-06 15:58:42 +0530

    Time: 2 ms (33.47%), Space: 42.4 MB (91.44%) - LeetHub

--- 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
+++ 3210-find-the-encrypted-string/3210-find-the-encrypted-string.java
@@ -1,8 +1,7 @@
 class Solution {
     public String getEncryptedString(String s, int k) {
-        int n = s.length();
-        k = k % n;
         
+        k = k % s.length();
         String partOne = s.substring(0, k);
         String partTwo = s.substring(k);
         
