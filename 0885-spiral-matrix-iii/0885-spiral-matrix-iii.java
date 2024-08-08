f8d8f44bd8d53038d1d2c26e44313c00c4c19718
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-08-08 23:54:01 +0530

    Time: 4 ms (81.31%), Space: 45.4 MB (21.96%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int directions[][] = {{0,1},{1,0},{0,-1},{-1,0}};
-        int n = rows*cols;
-        int res[][] = new int[n][2];//[[r,c],[r1,c1]...
-        res[0][0] = rStart;
-        res[0][1] = cStart;
+        int [][]dimensions = {{0,1},{1,0},{0,-1},{-1,0}};
+        int n=rows*cols;
+        int [][]ans = new int [n][2];
+        ans[0][0]=rStart;
+        ans[0][1]=cStart;
         int count=1;
         int step=1;
         int index=0;
-        
         while(count<n){
-            for(int times=0;times<2;times++){
-                int dr = directions[index%4][0];
-                int dc = directions[index%4][1];
-                for(int i=0;i<step;i++){
+            for(int i=0;i<2;i++){
+                int dr = dimensions[index%4][0];
+                int dc = dimensions[index%4][1];
+                for(int j=0;j<step;j++){
                     rStart+=dr;
                     cStart+=dc;
                     if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
-                        res[count][0] = rStart;
-                        res[count][1] = cStart;
+                        ans[count][0]=rStart;
+                        ans[count][1]=cStart;
                         count++;
                     }
                 }
@@ -26,6 +25,38 @@ class Solution {
             }
             step++;
         }
-        return res;
+        return ans;
     }
 }
+
+// class Solution {
+//     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+//         int directions[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+//         int n = rows*cols;
+//         int res[][] = new int[n][2];//[[r,c],[r1,c1]...
+//         res[0][0] = rStart;
+//         res[0][1] = cStart;
+//         int count=1;
+//         int step=1;
+//         int index=0;
+        
+//         while(count<n){
+//             for(int times=0;times<2;times++){
+//                 int dr = directions[index%4][0];
+//                 int dc = directions[index%4][1];
+//                 for(int i=0;i<step;i++){
+//                     rStart+=dr;
+//                     cStart+=dc;
+//                     if(rStart>=0 && rStart<rows && cStart>=0 && cStart<cols){
+//                         res[count][0] = rStart;
+//                         res[count][1] = cStart;
+//                         count++;
+//                     }
+//                 }
+//                 index++;
+//             }
+//             step++;
+//         }
+//         return res;
+//     }
+// }
