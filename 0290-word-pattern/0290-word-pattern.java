905fa80ed3f8921a0eb3cb4c7ee558011e941933
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-08-10 20:56:17 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (43.59%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,24 +1,35 @@
 class Solution {
-    public boolean wordPattern(String pattern, String s) {
-        HashMap<Character, String> map = new HashMap<>();
-        String[] arr = s.split(" ");
+//     public boolean wordPattern(String pattern, String s) {
+//         HashMap<Character, String> map = new HashMap<>();
+//         String[] arr = s.split(" ");
     
-        if (arr.length != pattern.length()) {
+//         if (arr.length != pattern.length()) {
+//             return false;
+//         }
+//         for (int i = 0; i < pattern.length(); i++) {
+//             if (!map.containsKey(pattern.charAt(i))) {
+//                 if (map.containsValue(arr[i])) {
+//                     return false; 
+//                 }
+//                 map.put(pattern.charAt(i), arr[i]);
+//             } else {
+//                 if (!map.get(pattern.charAt(i)).equals(arr[i])) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length())
             return false;
-        }
-    
-        for (int i = 0; i < pattern.length(); i++) {
-            if (!map.containsKey(pattern.charAt(i))) {
-                if (map.containsValue(arr[i])) {
-                    return false; 
-                }
-                map.put(pattern.charAt(i), arr[i]);
-            } else {
-                if (!map.get(pattern.charAt(i)).equals(arr[i])) {
-                    return false;
-                }
-            }
-        }
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
         return true;
     }
 }
