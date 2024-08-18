48d3df52de64ad84bb46adaa1d9fb926551238b2
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-08-18 20:00:20 +0530

    Time: 342 ms (63.21%), Space: 0B (100.00%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,5 @@
+select q.query_name, round(avg(q.rating/q.position), 2) 'quality', 
+round(avg(if(rating<3,1,0))*100,2) 'poor_query_percentage'
+from Queries q
+where q.query_name is not null
+group by q.query_name;
