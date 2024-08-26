b769ea9aa790edff445fe902bb7b3f949476e7c4
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-08-26 14:49:16 +0530

    Time: 1 ms (96.15%), Space: 44.8 MB (30.99%) - LeetHub

--- 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> result = new ArrayList<>();
-        dfs(root, result, 0);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, res, 0);
+        return res;
     }
 
-    private void dfs(Node root, List<List<Integer>> result, int level) {
+    private void dfs(Node root, List<List<Integer>> res, int level) {
         if(root == null)
             return;
 
-        if(result.size() <= level)
-            result.add(new ArrayList<>());
+        if(res.size() <= level)
+            res.add(new ArrayList<>());
 
-        result.get(level).add(root.val);
+        res.get(level).add(root.val);
 
         for(Node child : root.children)
-            dfs(child, result, level + 1);
+            dfs(child, res, level + 1);
     }
 }
