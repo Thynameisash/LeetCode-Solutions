0b04c9b21e2c3b0494b9d8f3b760adf7ed8bd4d5
Thynameisash <51952607+Thynameisash@users.noreply.github.com>
2024-09-02 22:56:19 +0530

    Time: 1537 ms (19.80%), Space: 60.1 MB (41.52%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,19 +1,13 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
+        if(chalk.length == 1) return 0;
         
-        int s = chalk.length;
-        if(s==1){
-            return 0;
-        }
         int i=0;
-        
         while(k>=chalk[i]){
             k-=chalk[i];
-            if(i==s-1){
-                i=i%(s-1);
-            } else {
-                i++;
-            }
+            if(i==chalk.length-1){
+                i=i%(chalk.length-1);
+            } else i++;
         }
         return i;
     }
